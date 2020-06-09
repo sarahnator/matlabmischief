8c4c99838829b6155d85c4be3b3babd5b2acd34d
Sarah Etter <sarahett@usc.edu>
2020-06-08 23:17:19 -0400

    cleaning

--- popArt/thresholding.m
+++ popArt/thresholding.m
@@ -1,19 +1,15 @@
 img = imread('wtfIcon.png');
-% imshow(img);
 
 % grayscale
-imgGray = rgb2gray(img);
-% imshow(imgGray);
+%  imgGray = rgb2gray(img);
 
-% % binary img 
+%  binary img 
 level = 0.6091;
 imgThresh  = imbinarize(imgGray,level);
 % imshowpair(img, imgThresh, 'montage');
-% 
 
 % use random color map function from imtool
 colorOps = {'jet', 'autumn', 'bone','colorcube', 'cool', 'copper', 'flag','gray', 'hot', 'lines', 'parula', 'pink', 'prism', 'spring','summer', 'winter'};
-
 op = string(colorOps(randi(size(colorOps,2))));
 imshow(imgThresh); 
 colormap(colormap(op));
