45aad32cbd33866f4d877d9d97712fa6f7bb422d
Sarah Etter <sarahett@usc.edu>
2020-06-11 18:09:35 -0400

    reading samples

--- audioProcessing/recording/recordingExample.m
+++ audioProcessing/recording/recordingExample.m
@@ -8,12 +8,21 @@
 % player = audioplayer(rec);
 % play(player);
 
+% ok, but matlab's audio actually sucks
+% rec = audiorecorder;
+% disp('Start speaking.')
+% recordblocking(rec,3);
+% disp('End of Recording.');
+% doubleArray = getaudiodata(rec);
+% plot(doubleArray);
+% title('Audio Signal (double)');
+% % sound(doubleArray)
 
-rec = audiorecorder;
-disp('Start speaking.')
-recordblocking(rec,3);
-disp('End of Recording.');
-doubleArray = getaudiodata(rec);
-plot(doubleArray);
-title('Audio Signal (double)');
-% sound(doubleArray)
+% import audacity files
+fileFolder = fullfile('wavFiles');
+% '*' for file pattern gives files + extra junk
+dirOutput = dir(fullfile(fileFolder,'*.wav'));
+for i=1:length(dirOutput)
+    disp(dirOutput(i).name)
+    % some processing here
+end
