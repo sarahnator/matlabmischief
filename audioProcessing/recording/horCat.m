31f11563867e3782b5f158292ff67d6fad9f8eb8
Sarah Etter <sarahett@usc.edu>
2020-06-19 17:28:25 -0400

    horCat in loop

--- audioProcessing/recording/horCat.m
+++ audioProcessing/recording/horCat.m
@@ -1,25 +1,19 @@
 addpath('wavFiles');
 % import audacity files
 fileFolder = fullfile('wavFiles');
-% '*' for file pattern gives files + extra junk
 dirOutput = dir(fullfile(fileFolder, '*.wav'));
 
-[multi_chan(:, 1), fs] = audioread(dirOutput(1).name);
-[multi_chan(:, 2), fs] = audioread(dirOutput(2).name);
-[multi_chan(:, 3), fs] = audioread(dirOutput(3).name);
-[multi_chan(:, 4), fs] = audioread(dirOutput(4).name);
-[multi_chan(:, 5), fs] = audioread(dirOutput(5).name);
-[multi_chan(:, 7), fs] = audioread(dirOutput(7).name);
-% There's apparently a size problem with chan 6 and chan 8 - rerecord...?
+numSignals = length(dirOutput);
 
-plot(multi_chan);
-title('6 Channel Horizontal Concatentation');
-legend({'chan1', 'chan2', 'chan3', 'chan4', 'chan5', 'chan7'}, 'location', 'southeast')
-saveas(gcf, 'horCat.png');
+% change first param based on num samples of signal if unknown length
+% or specify maximum size to prevent resizing
+multi_chan = zeros(100763, numSignals);
 
-% Matlab supports one & two channel audio
-% play single channel
-sound(multi_chan(:, 2), fs)
+for i=1:numSignals
+    % because there's a problem with 6 and 8
+    if i ~= 6 && i ~= 8
+         % disp(dirOutput(i).name)
+        [multi_chan(:, i)] = audioread(dirOutput(i).name);
+    end
+end
 
-% two channel-audio
-sound(multi_chan(:, 1:2), fs)
