606b9076125f4440eed79c87338d67a26cb78ca1
Sarah Etter <sarahett@usc.edu>
2020-06-19 16:46:27 -0400

    listen to 2 chan audio

--- audioProcessing/recording/horCat.m
+++ audioProcessing/recording/horCat.m
@@ -2,7 +2,7 @@ addpath('wavFiles');
 % import audacity files
 fileFolder = fullfile('wavFiles');
 % '*' for file pattern gives files + extra junk
-dirOutput = dir(fullfile(fileFolder,'*.wav'));
+dirOutput = dir(fullfile(fileFolder, '*.wav'));
 
 [multi_chan(:, 1), fs] = audioread(dirOutput(1).name);
 [multi_chan(:, 2), fs] = audioread(dirOutput(2).name);
@@ -14,5 +14,12 @@ dirOutput = dir(fullfile(fileFolder,'*.wav'));
 
 plot(multi_chan);
 title('6 Channel Horizontal Concatentation');
-legend({'chan1','chan2','chan3','chan4','chan5','chan7'},'location','southeast')
+legend({'chan1', 'chan2', 'chan3', 'chan4', 'chan5', 'chan7'}, 'location', 'southeast')
 saveas(gcf, 'horCat.png');
+
+% Matlab supports one & two channel audio
+% play single channel
+sound(multi_chan(:, 2), fs)
+
+% two channel-audio
+sound(multi_chan(:, 1:2), fs)
